9078539e7504f1202126025537c9bb7371a010c2
Alexandra Pereira <alesilva241@gmail.com>
2019-05-28 14:41:29 -0300

    tune phan config

--- .phan:config.php
+++ .phan:config.php
@@ -26,9 +26,85 @@ return [
     // your application should be included in this list.
     'directory_list' => [
         'app',
-        'vendor/symfony/console',
+    ],
+    // Backwards Compatibility Checking. This is slow
+    // and expensive, but you should consider running
+    // it before upgrading your version of PHP to a
+    // new version that has backward compatibility
+    // breaks. (Also see target_php_version)
+    'backward_compatibility_checks' => false,
+
+    // If true, this run a quick version of checks that takes less
+    // time at the cost of not running as thorough
+    // an analysis. You should consider setting this
+    // to true only when you wish you had more **undiagnosed** issues
+    // to fix in your code base.
+    'quick_mode' => false,
+
+    // If enabled, check all methods that override a
+    // parent method to make sure its signature is
+    // compatible with the parent's. This check
+    // can add quite a bit of time to the analysis.
+    'analyze_signature_compatibility' => false,
+
+    // The minimum severity level to report on. This can be
+    // set to Issue::SEVERITY_LOW(0), Issue::SEVERITY_NORMAL(5) or
+    // Issue::SEVERITY_CRITICAL(10). Setting it to only
+    // critical issues is a good place to start on a big
+    // sloppy mature code base.
+    'minimum_severity' => 10,
+
+    // If true, missing properties will be created when
+    // they are first seen. If false, we'll report an
+    // error message if there is an attempt to write
+    // to a class property that wasn't explicitly
+    // defined.
+    'allow_missing_properties' => true,
+
+    // Allow null to be cast as any type and for any
+    // type to be cast to null. Setting this to false
+    // will cut down on false positives.
+    'null_casts_as_any_type' => false,
+
+    // Allow null to be cast as any array-like type.
+    // This is an incremental step in migrating away from null_casts_as_any_type.
+    // If null_casts_as_any_type is true, this has no effect.
+    'null_casts_as_array' => false,
+
+    // Allow any array-like type to be cast to null.
+    // This is an incremental step in migrating away from null_casts_as_any_type.
+    // If null_casts_as_any_type is true, this has no effect.
+    'array_casts_as_null' => false,
+
+    // If enabled, scalars (int, float, bool, true, false, string, null)
+    // are treated as if they can cast to each other.
+    'scalar_implicit_cast' => false,
+
+    // If this has entries, scalars (int, float, bool, true, false, string, null)
+    // are allowed to perform the casts listed.
+    // E.g. ['int' => ['float', 'string'], 'float' => ['int'], 'string' => ['int'], 'null' => ['string']]
+    // allows casting null to a string, but not vice versa.
+    // (subset of scalar_implicit_cast)
+    'scalar_implicit_partial' => [],
+
+    // If true, seemingly undeclared variables in the global
+    // scope will be ignored. This is useful for projects
+    // with complicated cross-file globals that you have no
+    // hope of fixing.
+    'ignore_undeclared_variables_in_global_scope' => false,
+
+    // Add any issue types (such as 'PhanUndeclaredMethod')
+    // to this black-list to inhibit them from being reported.
+    'suppress_issue_types' => [
+        // 'PhanUndeclaredMethod',
     ],
 
+    // If empty, no filter against issues types will be applied.
+    // If this white-list is non-empty, only issues within the list
+    // will be emitted by Phan.
+    'whitelist_issue_types' => [
+        // 'PhanAccessMethodPrivate',
+    ],
     // A regex used to match every file name that you want to
     // exclude from parsing. Actual value will exclude every
     // "test", "tests", "Test" and "Tests" folders found in
@@ -47,6 +123,7 @@ return [
     //       should be added to both the `directory_list`
     //       and `exclude_analysis_directory_list` arrays.
     'exclude_analysis_directory_list' => [
-        'vendor/'
+        'vendor/',
+        'test/'
     ],
 ];
